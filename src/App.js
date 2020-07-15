679398789958b50f67e12704dc0d4608b8d58139
darge <mattia.darge@gmail.com>
2020-07-15 14:54:17 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
 import { Loader } from "./components/loader/Loader";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Delay manuale
+const MainLayout = React.lazy(() => {
+  return Promise.all([
+    import("./layout/main/MainLayout"),
+    new Promise((resolve) => setTimeout(resolve, 2500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
 
 function App() {
   return (
     <div className="App">
-      <Loader />
+      <React.Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/">
+            <MainLayout />
+          </Route>
+        </Switch>
+      </React.Suspense>
     </div>
   );
 }
---
+++ src/layout/main/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Sidebar } from "../../components/sidebar/Sidebar";
+
+const MainLayout = () => (
+  <div className="main-layout d-flex flex-column h-100 w-100">
+    <Sidebar />
+    asd
+  </div>
+);
+
+export default MainLayout;
